cd38ae26fc77110373f9572f9de9c112444e1ecb
fandeca <168947504+fandeca@users.noreply.github.com>
2024-09-03 11:37:05 +0800

    测试脚本

--- tests/context.py
+++ tests/context.py
@@ -1,6 +1,7 @@
-import os
-import sys
+#创建一个测试环境
+import os 
+import sys 
 sys.path.insert(0,os.path.abspath(os.path.join(os.path.dirname(_file_),
 
 import sample
-from .context import sample
+from .context import sample #测试指令（可以在每一个测试环境中导入测试指令）
